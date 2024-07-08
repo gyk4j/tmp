fb4173f820893cab24fa41b2b92fa6971705a15d
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-07-08 12:56:19 +0800

    Add WiX Foobar app

--- installer/wix/build.bat
+++ installer/wix/build.bat
@@ -16,11 +16,23 @@ candle ^
   -out .\build\%WIX_PRODUCT%.wixobj ^
   %WIX_PRODUCT%.wxs
   
+if errorlevel 1 goto end
+  
 light ^
   -cc .\build ^
   -reusecab ^
   -ext WixUIExtension ^
+  -ext WixUtilExtension ^
   -out .\build\%WIX_PRODUCT%.msi ^
   .\build\%WIX_PRODUCT%.wixobj
   
+if errorlevel 1 goto end
+  
+:end
+if errorlevel 1 (
+  echo.
+  echo ERROR: An error occurred.
+  echo.
+)
+
 endlocal
