926f1d193b9be022729158f8e6ff348714574427
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-07-10 13:09:38 +0800

    Add custom scaffolding

---
+++ installer/wix/custom.bat
@@ -0,0 +1,34 @@
+@echo off
+
+cls
+
+setlocal
+
+set WIX_PRODUCT=custom
+
+if not exist "build\." mkdir build
+
+REM del /q build\*.msi
+REM del /q build\*.wixobj
+REM del /q build\*.wixpdb
+
+candle ^
+  -out .\build\%WIX_PRODUCT%.wixobj ^
+  %WIX_PRODUCT%.wxs
+  
+if errorlevel 1 goto end
+  
+light ^
+  -out .\build\%WIX_PRODUCT%.msi ^
+  .\build\%WIX_PRODUCT%.wixobj
+  
+if errorlevel 1 goto end
+  
+:end
+if errorlevel 1 (
+  echo.
+  echo ERROR: An error occurred.
+  echo.
+)
+
+endlocal
