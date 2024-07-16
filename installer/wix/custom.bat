183c1ba4c221f62838e708b4cbb9d9995fd77961
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-07-16 13:31:49 +0800

    Split custom dialog into separate files

--- installer/wix/custom.bat
+++ installer/wix/custom.bat
@@ -12,10 +12,13 @@ REM del /q build\*.msi
 REM del /q build\*.wixobj
 REM del /q build\*.wixpdb
 
+candle -nologo -out build\cuidlga.wixobj cuidlga.wxs
+candle -nologo -out build\cuidlgb.wixobj cuidlgb.wxs
+
 candle ^
   -nologo ^
   -out .\build\%WIX_PRODUCT%.wixobj ^
-  %WIX_PRODUCT%.wxs
+  %WIX_PRODUCT%.wxs 
   
 if errorlevel 1 goto end
   
@@ -23,7 +26,7 @@ light ^
   -nologo ^
   -ext WixUIExtension ^
   -out .\build\%WIX_PRODUCT%.msi ^
-  .\build\%WIX_PRODUCT%.wixobj
+  .\build\%WIX_PRODUCT%.wixobj .\build\cuidlga.wixobj .\build\cuidlgb.wixobj
   
 if errorlevel 1 goto end
   
