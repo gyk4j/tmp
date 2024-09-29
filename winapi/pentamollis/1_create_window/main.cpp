a802eac489e67d9363b7db27d7c4f8970d48d85f
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-09-29 14:27:51 +0800

    Add create window

---
+++ winapi/pentamollis/1_create_window/main.cpp
@@ -0,0 +1,54 @@
+#include <windows.h>
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int nCmdShow)
+{
+    WNDCLASSW wc = {0};
+    
+    wc.hbrBackground = (HBRUSH) COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"myWindowClass";
+    wc.lpfnWndProc = WindowProcedure;
+    
+    if(!RegisterClassW(&wc))
+        return -1;
+    
+    CreateWindowW
+    (
+        L"myWindowClass",                   // Registered Window class
+        L"My Window",                       // Window title
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,   // Window style(s)
+        100,                                // Position left X
+        100,                                // Position top Y
+        500,                                // Window Width
+        500,                                // Window Height
+        NULL,                               // Parent window handle
+        NULL,                               // Menu handle
+        NULL,                               // Application handle (set in wc)
+        NULL                                // Other parameters...
+    );
+    
+    MSG msg = {0};
+    
+    while( GetMessage(&msg, NULL, NULL, NULL) )
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    
+    return 0;
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProcW(hWnd, msg, wp, lp);
+    }
+}
