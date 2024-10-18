7957a878236430a24304f47aef259529fe266cb1
gyk <147011991+gyk4j@users.noreply.github.com>
2024-10-18 13:01:55 +0800

    Add shell environment variable interpolation test (#73)

--- docker/env-var-test/main.go
+++ docker/env-var-test/main.go
@@ -12,4 +12,9 @@ func main() {
         os.Exit(1)
     }
     fmt.Printf("Hostname: %s\n", hostname)
+    
+    fmt.Printf("SHELL: %s\n", os.Getenv("SHELL"))
+    fmt.Printf("TERM: %s\n", os.Getenv("TERM"))
+    fmt.Printf("USER: %s\n", os.Getenv("USER"))
+    fmt.Printf("HOME: %s\n", os.Getenv("HOME"))
 }
