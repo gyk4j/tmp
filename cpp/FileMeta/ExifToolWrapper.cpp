c526bb3c279ac4005d0b4dbb26ffca2bc12a2ae1
gyk <147011991+gyk4j@users.noreply.github.com>
2024-11-16 10:19:11 +0800

    Move GetErrorMessage helper function into ExifToolWrapper namespace (#120)

--- cpp/FileMeta/ExifToolWrapper.cpp
+++ cpp/FileMeta/ExifToolWrapper.cpp
@@ -425,25 +425,25 @@ namespace ExifToolWrapper
         
         return true;
     }
-}
-
-BOOL GetErrorMessage(DWORD dwErrorCode, LPTSTR pBuffer, DWORD cchBufferLength)
-{
-    if (cchBufferLength == 0)
-    {
-    return FALSE;
-    }
     
-    DWORD cchMsg = FormatMessage(
-        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-        NULL,  /* (not used with FORMAT_MESSAGE_FROM_SYSTEM) */
-        dwErrorCode,
-        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        pBuffer,
-        cchBufferLength,
-        NULL);
+    BOOL GetErrorMessage(DWORD dwErrorCode, LPTSTR pBuffer, DWORD cchBufferLength)
+    {
+        if (cchBufferLength == 0)
+        {
+        return FALSE;
+        }
         
-    return (cchMsg > 0);
+        DWORD cchMsg = FormatMessage(
+            FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+            NULL,  /* (not used with FORMAT_MESSAGE_FROM_SYSTEM) */
+            dwErrorCode,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            pBuffer,
+            cchBufferLength,
+            NULL);
+            
+        return (cchMsg > 0);
+    }
 }
 
 void TestParsing();
